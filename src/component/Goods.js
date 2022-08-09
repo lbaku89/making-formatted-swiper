c3b7a9402ec0fdfec75d827f8197cefd3e8ab7ba
khw <khj930410@naver.com>
2022-08-09 12:56:00 +0900

    feat : skinfood style 삽입형 ui 완료

--- src/component/Goods.js
+++ src/component/Goods.js
@@ -1,35 +1,35 @@
 import React from "react";
 
 export function Goods(props) {
-  // 원 단위로 환산
-  let KRW = props.data.goodsPrice
-    .toString()
-    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
-  return (
-    <div className="w-full h-full max-width-240px">
-      <a
-        href={props.data.goodsUrl}
-        target="_blank"
-        className="block"
-        rel="noreferrer"
-      >
-        <div className="">
-          <img
-            className="block w-full"
-            src={props.data.goodsImgUrl}
-            alt="#none"
-            id="goodsImg"
-          />
-        </div>
-        <div className="w-full py-4 px-1">
-          <p title="${data.goodsName}" className="" id="goodsName">
-            {props.data.goodsName}
-          </p>
-          <p className="" id="goodsPrice">
-            &#8361;{KRW}
-          </p>
-        </div>
-      </a>
-    </div>
-  );
+	// 원 단위로 환산
+	let KRW = props.data.goodsPrice
+		.toString()
+		.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+	return (
+		<div className="w-full h-full max-width-240px">
+			<a
+				href={props.data.goodsUrl}
+				target="_blank"
+				className="block"
+				rel="noreferrer"
+			>
+				<div className="">
+					<img
+						className="block w-full"
+						src={props.data.goodsImgUrl}
+						alt="상품IMG URL"
+						id="goodsImg"
+					/>
+				</div>
+				<div className="w-full py-[16px] px-[4px]">
+					<p title="${data.goodsName}" className="" id="goodsName">
+						{props.data.goodsName}
+					</p>
+					<p className="" id="goodsPrice">
+						&#8361;{KRW}
+					</p>
+				</div>
+			</a>
+		</div>
+	);
 }
