df44e57e282e84a50263d4fa4851998d430443c1
khw <khj930410@naver.com>
2022-08-16 13:06:52 +0900

    feat : thunk-test 완료 / 구현방식 설정

--- src/store/thunkFetchStyleProperty.js
+++ src/store/thunkFetchStyleProperty.js
@@ -2,18 +2,18 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const thunkFetchStyleProperty = createAsyncThunk(
-  "thunkFetchStyle",
-  async () => {
-    const styleProperty = await axios
-      .get("http://localhost:3001/midasB")
-      .then((response) => {
-        return response.data;
-      })
-      .catch((err) => {});
+	"thunkFetchStyle",
+	async () => {
+		const styleProperty = await axios
+			.get("http://localhost:3001/midasB")
+			.then((response) => {
+				return response.data;
+			})
+			.catch((err) => {});
 
-    console.log("fetchAPI=>", styleProperty);
-    return styleProperty;
-  }
+		console.log("fetchAPI=>", styleProperty);
+		return styleProperty;
+	}
 );
 
 export { thunkFetchStyleProperty };
